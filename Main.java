02af334048694f142376ea28e82df438e16f7dec
asiermunoz <asiermunozlandi@gmail.com>
2024-10-15 21:55:36 -0400

    login both users

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] arg){
-        System.out.println("Hola mundo");
+    public static void main(String[] arg) {
+        Scanner read = new Scanner(System.in);
+        String user1, user2;
+        String mail1, mail2;
+        //RUN menu
+        System.out.println("\t\t MENU LOGIN USERS: \n");
+        System.out.println("User 1: ");
+        user1 = read.next();
+        System.out.println("user 1 mail : ");
+        mail1 = read.next();
+        System.out.println("User 2: ");
+        user2 = read.next();
+        System.out.println("user 2 mail: ");
+        mail2 = read.next();
+
+
+
+        /*
+         * no hace un cls en la terminal
+         * no hace un pause en la terminal
+         * no tiene ninguna funcion incluida
+         * */
     }
 }
+
