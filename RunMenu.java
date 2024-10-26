16fc62f7d80fd0f4e13077d63bd6ca2ec66a5e90
JD2910 <165451598+JD2910@users.noreply.github.com>
2024-10-26 14:36:14 -0400

    Add files via upload
    Clase con el menú inicial del juego

--- RunMenu.java
+++ RunMenu.java
@@ -1,33 +1,46 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class RunMenu {
-    Scanner read = new Scanner(System.in);
-    private int counter = -1;
 
-    public RunMenu() {
+    //Para poner colores en el texto de la consola.
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    //Para poner colores de fondo del texto de la consola.
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    //Borra cualquier color y deja el color por defecto de la consola.
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    //Menú principal.
+    public static int menu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(ANSI_YELLOW_BACKGROUND + ANSI_BLACK + "---------SCRABBLE-UCAB---------" + ANSI_RESET);
+        System.out.println(ANSI_YELLOW + "Ingrese el número de la acción que desee realizar:" + ANSI_RESET);
+        System.out.println("1. Jugar nueva Partida.");
+        System.out.println("2. Continuar partida anterior.");
+        System.out.println("3. Ver Score de los jugadores.");
+        System.out.println("4. Salir.");
+        return scanner.nextInt();
     }
-
-    public int getCounter() {
-        return counter;
-    }
-
-    public void setCounter(int counter) {
-        this.counter = counter;
-    }
-
-    public void runMenu(){
-        while (counter != 0){
-            System.out.println("\t\t MENU SCRABBLE: \n");
-            System.out.println("0. Exit");
-            System.out.println("1. New Game");
-            System.out.println("2. Continue Last Game");
-            System.out.println("Settings");
-            System.out.println("Player Score");
-
-            System.out.println("\n insert your option: ");
-            counter = read.nextInt();
-
-            switch (counter){
+    public static void main(String[] args) throws InterruptedException, IOException {
+        int option;
+        do {
+            option = menu();
+            switch(option){
                 case 1:
                     break;
                 case 2:
@@ -35,10 +48,15 @@ public class RunMenu {
                 case 3:
                     break;
                 case 4:
+                    System.out.println(ANSI_BLUE + "Saliendo del juego...");
+                    Thread.sleep(800); //Pausa la corrida de código por los milisegundos establecidos.
+                    System.out.println(ANSI_YELLOW + "Vuelva pronto!");
+                    break;
+                default:
+                    System.out.println(ANSI_RED + "El número ingresado no posee acción alguna\n");
+                    Thread.sleep(1500); //Pausa la corrida de código por los milisegundos establecidos.
                     break;
             }
-
-
-        }
+        }while(option != 4);
     }
 }
