23ad9d70f3fdc6bb1b6dfa3eb1056879b3906244
JD2910 <165451598+JD2910@users.noreply.github.com>
2024-11-11 20:43:29 -0400

    Add files via upload
    - Updated the color class to be abstract.

--- ucab/edu/objects/Color.java
+++ ucab/edu/objects/Color.java
@@ -1,6 +1,6 @@
 package ucab.edu.objects;
 
-public class Color {
+public abstract class Color {
     //Para poner colores en el texto de la consola.
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
