315e598b60324068e7a4bc561e6f6c08de5ca952
JD2910 <165451598+JD2910@users.noreply.github.com>
2024-11-22 15:13:48 -0400

    Game table

---
+++ Game app/src/ucab/edu/objects/Board.java
@@ -0,0 +1,70 @@
+package ucab.edu.objects;
+
+public class Board {
+    private String[][] table = new String[15][15];
+
+    public Board(String[][] table) {
+        this.table = table;
+    }
+
+    public String[][] getTable() {
+        return table;
+    }
+
+    public void setTable(String[][] table) {
+        this.table = table;
+    }
+
+    public void printTable(){
+        System.out.printf("%3s"," ");
+        for(int i = 0; i< table.length; i++){
+            System.out.printf("%3s",i);
+            System.out.printf("%s"," ");
+        }
+        System.out.println();
+        for (int i = 0; i < table.length; i++){
+            System.out.printf("%2s",(char)(65+i));
+            System.out.printf("%s"," ");
+            for (int j = 0; j < 15; j++){
+                System.out.printf("|%2s",table[i][j]);
+                System.out.printf("%s"," ");
+            }
+            System.out.printf("|%n");
+        }
+    }
+
+    public void emptyTable() {
+        for (int i = 0; i < table.length; i++){
+            for (int j = 0; j < table.length; j++){
+                table[i][j]=" ";
+            }
+        }
+    }
+
+    public boolean verifyCoordinateX(int coordinate) {
+        try {
+            if (coordinate >= 0 && coordinate <= table.length) {
+                return true;
+            }
+            throw new OutOfDimensionsException();
+        }catch(OutOfDimensionsException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean verifyCoordinateY(char coordinate) {
+        try {
+            if ((int)(coordinate) >= 65 && (int)(coordinate) <= (65 + table.length)) {
+                return true;
+            }
+            throw new OutOfDimensionsException();
+        }catch(OutOfDimensionsException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+
+
+}
