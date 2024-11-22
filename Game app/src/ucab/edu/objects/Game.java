71f07b3e96b2af481c0724ef5dd9568d64395773
JD2910 <165451598+JD2910@users.noreply.github.com>
2024-11-22 15:26:01 -0400

    Possible actions on the game.

---
+++ Game app/src/ucab/edu/objects/Game.java
@@ -0,0 +1,23 @@
+package ucab.edu.objects;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Game {
+    public boolean endGame(Order order, Player turn){
+        Scanner select = new Scanner(System.in);
+        int opc;
+        for(Player next: order.getPlayers()){
+            if(next != turn){
+                System.out.println("El jugador " + next.getAlias() + " desea terminar igualmente la partida?");
+                System.out.println("1. Sí");
+                System.out.println("0. No");
+                opc = select.nextByte();
+                if(opc == 1){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
